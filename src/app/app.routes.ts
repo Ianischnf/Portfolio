14248946d185568f9d21d7ecc54efad4c76f225d
Ianischnf <81810701+Ianischnf@users.noreply.github.com>
2026-03-16 16:42:03 +0100

    page projet

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { Acceuil } from './pages/acceuil/acceuil';
+import { Projets } from './pages/projets/projets';
+import { Certificats } from './pages/certificats/certificats';
+import { Contact } from './pages/contact/contact';
 
-export const routes: Routes = [];
+
+
+export const routes: Routes = [
+    {path: '', component: Acceuil },
+    {path: 'acceuil', component: Acceuil },
+    {path: 'projets', component: Projets },
+    {path: 'certificats', component: Certificats },
+    {path: 'contact', component: Contact }
+];
--- src/app/pages/projets/projets.ts
+++ src/app/pages/projets/projets.ts
@@ -1,11 +1,57 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Menu } from "../../components/menu/menu";
+import { Projet } from "../../components/projet/projet";
+import { NgFor, NgForOf } from '@angular/common';
 
 @Component({
   selector: 'app-projets',
-  imports: [],
+  imports: [Menu, Projet, NgFor],
   templateUrl: './projets.html',
   styleUrl: './projets.css',
 })
 export class Projets {
 
+  Projets = [
+
+    {
+      ImageProjet: "assets/coiffeur_manage.png",
+      NomProjet: "Gestion rendez-vous",
+      DescriptionProjet: "Application gestion de rendez-vous pour coiffeur",
+      StackProjet: "Stack : Angular | Java Spring Boot",
+      IconGithub: "assets/sociale.png",
+      IconLien: "assets/lien.png",
+      UrlGithub : "https://github.com/Ianischnf/Coiffeur_manage_front.git",
+      UrlProjet : ''
+    },
+    {
+      ImageProjet: "assets/product.png",
+      NomProjet: "Liste de produits et gestion de panier",
+      DescriptionProjet: "Application gestion de panier",
+      StackProjet: "Stack : React",
+      IconGithub: "assets/sociale.png",
+      IconLien: "assets/lien.png",
+      UrlGithub : "https://github.com/Ianischnf/ListProductsCard.git",
+      UrlProjet : ''
+    },
+    {
+      ImageProjet: "assets/ext1.png",
+      NomProjet: "Gestion extension navigateur",
+      DescriptionProjet: "Activer/Désactiver extension et filtrer l'affichage selon leur état",
+      StackProjet: "Stack : React",
+      IconGithub: "assets/sociale.png",
+      IconLien: "assets/lien.png",
+      UrlGithub : "https://github.com/Ianischnf/Browser_extensions.git",
+      UrlProjet : ''
+    },
+    {
+      ImageProjet: "assets/Meteo.png",
+      NomProjet: "Météo",
+      DescriptionProjet: "Application météo",
+      StackProjet: "Stack : HTML / CSS / JavaScript",
+      IconGithub: "assets/sociale.png",
+      IconLien: "assets/lien.png",
+      UrlGithub : "https://github.com/Ianischnf/Meteo_App.git",
+      UrlProjet : ''
+    }
+  ]
 }
